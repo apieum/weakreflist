db5bd6786fd155707bead4a9aa946b3f226c8bbf
Grégory Salvan <apieum@gmail.com>
2014-11-27 19:03:15 +0100

    Refactor: rename make_ref in ref and get_value in value

--- weakreflist/weakreflist.py
+++ weakreflist/weakreflist.py
@@ -12,63 +12,63 @@ class WeakList(list):
     def __init__(self, items=list()):
         list.__init__(self, self._refs(items))
 
-    def get_value(self, item):
+    def value(self, item):
         return item() if isinstance(item, ReferenceType) else item
 
-    def make_ref(self, item):
+    def ref(self, item):
         try:
             item = ref(item, self.remove_all)
         finally:
             return item
 
     def __contains__(self, item):
-        return list.__contains__(self, self.make_ref(item))
+        return list.__contains__(self, self.ref(item))
 
     def __getitem__(self, index):
         items = list.__getitem__(self, index)
-        return type(self)(self._values(items)) if is_slice(index) else self.get_value(items)
+        return type(self)(self._values(items)) if is_slice(index) else self.value(items)
 
     def __setitem__(self, index, item):
-        items = self._refs(item) if is_slice(index) else self.make_ref(item)
+        items = self._refs(item) if is_slice(index) else self.ref(item)
         return list.__setitem__(self, index, items)
 
     def __iter__(self):
         return iter(self[index] for index in range(len(self)))
 
     def append(self, item):
-        list.append(self, self.make_ref(item))
+        list.append(self, self.ref(item))
 
     def remove(self, item):
-        return list.remove(self, self.make_ref(item))
+        return list.remove(self, self.ref(item))
 
     def remove_all(self, item):
-        item = self.make_ref(item)
+        item = self.ref(item)
         while list.__contains__(self, item):
             list.remove(self, item)
 
     def index(self, item):
-        return list.index(self, self.make_ref(item))
+        return list.index(self, self.ref(item))
 
     def count(self, item):
-        return list.count(self, self.make_ref(item))
+        return list.count(self, self.ref(item))
 
     def pop(self, item):
-        return list.pop(self, self.make_ref(item))
+        return list.pop(self, self.ref(item))
 
     def insert(self, index, item):
-        return list.insert(self, index, self.make_ref(item))
+        return list.insert(self, index, self.ref(item))
 
     def extend(self, items):
         return list.extend(self, self._refs(items))
 
     def _refs(self, items):
-        return map(self.make_ref, items)
+        return map(self.ref, items)
 
     def _values(self, items):
-        return map(self.get_value, items)
+        return map(self.value, items)
 
     def _sort_key(self, key=None):
-        return self.get_value if key == None else lambda item: key(self.get_value(item))
+        return self.value if key == None else lambda item: key(self.value(item))
 
     if sys.version_info < (3,):
         def sort(self, cmp=None, key=None, reverse=False):
