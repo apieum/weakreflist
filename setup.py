6bf5dcd85678103279d4148b8036bddf4eeaedf7
Grégory Salvan <apieum@gmail.com>
2017-04-08 00:47:15 +0200

    setting version 0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 pkgName = 'weakreflist'
 setup(
     name=pkgName,
-    version='0.3.1',
+    version='0.4',
     url='http://www.python.org/pypi/' + pkgName,
     author='Grégory Salvan',
     author_email='apieum@gmail.com',
